052163aa24b36d4d1b1df94fbad3d4dd711f7587
Perry Son <13183804+perrylson@users.noreply.github.com>
2025-08-25 00:37:27 -0700

    add cyw43 firmware files

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,9 @@ use {defmt_rtt as _, panic_probe as _};
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
     let _periphs = embassy_rp::init(Default::default());
+    let fw = include_bytes!("../cyw43/43439A0.bin");
+    let clm = include_bytes!("../cyw43/43439A0_clm.bin");
+
     loop {
         defmt::info!("Blink");
         Timer::after(Duration::from_millis(100)).await;
