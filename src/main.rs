c27b62a1eb465002ac7649d753c20ce93e1230d5
Perry Son <13183804+perrylson@users.noreply.github.com>
2025-08-26 00:35:28 -0700

    add implementation of DPS310 driver struct

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 #![no_std]
 #![no_main]
 
+use defmt::*;
 use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::i2c::Config;
 use embassy_time::{Duration, Timer};
+mod sensor;
+use crate::sensor::sensor::{DPS310, OversampleRate};
+use cyw43::JoinOptions;
+use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
+use embassy_net::StackResources;
+use embassy_rp::clocks::RoscRng;
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler, Pio};
+use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
+
+
+bind_interrupts!(struct Irqs {
+        I2C1_IRQ => embassy_rp::i2c::InterruptHandler<embassy_rp::peripherals::I2C1>;
+        PIO0_IRQ_0 => InterruptHandler<PIO0>;
+});
+
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) -> ! {
@@ -11,8 +31,20 @@ async fn main(_spawner: Spawner) -> ! {
     let fw = include_bytes!("../cyw43/43439A0.bin");
     let clm = include_bytes!("../cyw43/43439A0_clm.bin");
 
-    loop {
-        defmt::info!("Blink");
-        Timer::after(Duration::from_millis(100)).await;
-    }
+    let sda = p.PIN_14;
+    let scl = p.PIN_15;
+    let mut i2c = embassy_rp::i2c::I2c::new_async(p.I2C1, scl, sda, Irqs, Config::default());
+
+    Timer::after(Duration::from_millis(100)).await;
+
+    let mut dps310 = DPS310::new(&mut i2c).await;
+    dps310
+        .set_temperature_oversampling_rate(OversampleRate::Eight)
+        .await;
+    dps310
+        .set_pressure_oversampling_rate(OversampleRate::Eight)
+        .await;
+
+    dps310.read_temperature().await;
+    //dps310.read_pressure().await;
 }
--- src/sensor.rs
+++ src/sensor.rs
@@ -1,5 +1,8 @@
+#![allow(dead_code)]
 pub mod sensor {
+    use defmt::info;
     use embassy_rp::{i2c::Async, peripherals::I2C1};
+    use embassy_time::{Duration, Timer};
     const SENSOR_ADDR: u8 = 0x77;
     const PROD_ID_REG_ADDR: u8 = 0x0D;
     const COEFFS_ADDR: u8 = 0x10;
@@ -22,25 +25,123 @@ pub mod sensor {
             casted_value
         }
     }
+    pub enum OversampleRate {
+        One,
+        Two,
+        Four,
+        Eight,
+        Sixteen,
+        ThirtyTwo,
+        SixtyFour,
+        OneHundredTwentyEight,
+    }
 
     pub struct DPS310<'a> {
         i2c: &'a mut embassy_rp::i2c::I2c<'a, I2C1, Async>,
-        coeffs: [i32; 9],
+        c0: i32,
+        c1: i32,
+        c00: i32,
+        c10: i32,
+        c01: i32,
+        c11: i32,
+        c20: i32,
+        c21: i32,
+        c30: i32,
+        selected_temperature_scale_factor: i32,
+        selected_pressure_scale_factor: i32,
     }
     impl<'a> DPS310<'a> {
         pub async fn new(i2c: &'a mut embassy_rp::i2c::I2c<'a, I2C1, Async>) -> DPS310<'a> {
             let mut dps310_sensor = DPS310 {
                 i2c,
-                coeffs: [0; 9],
+                c0: 0,
+                c1: 0,
+                c00: 0,
+                c10: 0,
+                c01: 0,
+                c11: 0,
+                c20: 0,
+                c21: 0,
+                c30: 0,
+                selected_temperature_scale_factor: SCALE_FACTORS[2],
+                selected_pressure_scale_factor: SCALE_FACTORS[2],
             };
+            dps310_sensor.initialize_sensor().await;
             dps310_sensor
+        }
+
+        pub async fn reset_sensor(&mut self) {
+            self.i2c
+                .write_async(SENSOR_ADDR, [0x0C as u8, 0x9 as u8])
+                .await
+                .unwrap();
+        }
+
+        pub async fn set_temperature_oversampling_rate(&mut self, rate: OversampleRate) {
+            let (reg_value, scale_factor) = match rate {
+                OversampleRate::One => (0x80, SCALE_FACTORS[0]),
+                OversampleRate::Two => (0x81, SCALE_FACTORS[1]),
+                OversampleRate::Four => (0x82, SCALE_FACTORS[2]),
+                OversampleRate::Eight => (0x83, SCALE_FACTORS[3]),
+                OversampleRate::Sixteen => todo!(),
+                OversampleRate::ThirtyTwo => todo!(),
+                OversampleRate::SixtyFour => todo!(),
+                OversampleRate::OneHundredTwentyEight => todo!(),
+            };
+            self.i2c
+                .write_async(SENSOR_ADDR, [TEMP_CONFIG_ADDR, reg_value])
+                .await
+                .unwrap();
+            self.selected_temperature_scale_factor = scale_factor;
+        }
+
+        pub async fn set_pressure_oversampling_rate(&mut self, rate: OversampleRate) {
+            let (reg_value, scale_factor) = match rate {
+                OversampleRate::One => (0x00, SCALE_FACTORS[0]),
+                OversampleRate::Two => (0x01, SCALE_FACTORS[1]),
+                OversampleRate::Four => (0x02, SCALE_FACTORS[2]),
+                OversampleRate::Eight => (0x03, SCALE_FACTORS[3]),
+                OversampleRate::Sixteen => todo!(),
+                OversampleRate::ThirtyTwo => todo!(),
+                OversampleRate::SixtyFour => todo!(),
+                OversampleRate::OneHundredTwentyEight => todo!(),
+            };
+            self.i2c
+                .write_async(SENSOR_ADDR, [PRESSURE_CONFIG_ADDR, reg_value])
+                .await
+                .unwrap();
+            self.selected_pressure_scale_factor = scale_factor;
+        }
+
+        pub async fn initialize_sensor(&mut self) {
+            let res = self.read_product_id().await;
+
+            let mut temp_coef: [u8; 1] = [0];
+            self.i2c
+                .write_read_async(SENSOR_ADDR, [0x28 as u8], &mut temp_coef)
+                .await
+                .unwrap();
+
+            self.initialize_coeffs().await;
+
+            self.i2c
+                .write_async(SENSOR_ADDR, [PRESSURE_CONFIG_ADDR, 0x02 as u8])
+                .await
+                .unwrap();
+            self.i2c
+                .write_async(SENSOR_ADDR, [TEMP_CONFIG_ADDR, 0x82 as u8])
+                .await
+                .unwrap();
+            self.i2c
+                .write_async(SENSOR_ADDR, [CFG_REG_ADDR, 0])
+                .await
+                .unwrap();
         }
 
         async fn check_temperature_status(&mut self) -> Result<&'static str, &'static str> {
             let mut meas_data: [u8; 1] = [0];
             let mut count = 0;
             let mut is_ready = false;
-
             while !is_ready && count < 5 {
                 self.i2c
                     .write_read_async(SENSOR_ADDR, [MEAS_CFG_ADDR], &mut meas_data)
@@ -65,6 +166,33 @@ pub mod sensor {
             }
         }
 
+        pub async fn read_temperature(&mut self) {
+            self.i2c
+                .write_async(SENSOR_ADDR, [MEAS_CFG_ADDR, 0x02 as u8])
+                .await
+                .unwrap();
+
+            self.check_temperature_status().await.unwrap();
+
+            let mut temperature_data: [u8; 3] = [0; 3];
+            self.i2c
+                .write_read_async(SENSOR_ADDR, [TEMP_ADDR], &mut temperature_data)
+                .await
+                .unwrap();
+            let raw_temp = get_twos_complement(
+                ((temperature_data[0] as u32) << 16)
+                    + ((temperature_data[1] as u32) << 8)
+                    + (temperature_data[2] as u32),
+                24,
+            );
+            info!("raw_temp: {} ", raw_temp);
+            let proc_temp = (self.c0 as f64) * 0.5
+                + (self.c1 as f64)
+                    * ((raw_temp as f64) / (self.selected_temperature_scale_factor as f64));
+            let temp_fahrenheit = (proc_temp * 9.0 / 5.0) + 32.0;
+            info!("Celsius: {}  Fahrenheit: {}", proc_temp, temp_fahrenheit);
+        }
+
         pub async fn read_product_id(&mut self) -> Result<&'static str, &'static str> {
             let mut result = [0];
             self.i2c
@@ -76,6 +204,45 @@ pub mod sensor {
             } else {
                 Err("Could not find product id")
             }
+        }
+
+        pub async fn initialize_coeffs(&mut self) {
+            let mut result: [u8; 18] = [0; 18];
+
+            self.i2c
+                .write_read_async(SENSOR_ADDR, [COEFFS_ADDR], &mut result)
+                .await
+                .unwrap();
+
+            self.c0 = get_twos_complement(
+                ((result[0] as u32) << 4) + (((result[1] as u32) >> 4) & 0x0F),
+                12,
+            );
+
+            self.c1 =
+                get_twos_complement((((result[1] as u32) & 0x0F) << 8) + (result[2] as u32), 12);
+
+            self.c00 = get_twos_complement(
+                ((result[3] as u32) << 12)
+                    + ((result[4] as u32) << 4)
+                    + (((result[5] as u32) >> 4) & 0x0F),
+                20,
+            );
+
+            self.c10 = get_twos_complement(
+                ((result[5] as u32) << 16) + ((result[6] as u32) << 8) + (result[7] as u32),
+                20,
+            );
+
+            self.c01 = get_twos_complement(((result[8] as u32) << 8) + (result[9] as u32), 16);
+
+            self.c11 = get_twos_complement(((result[10] as u32) << 8) + (result[11] as u32), 16);
+
+            self.c20 = get_twos_complement(((result[12] as u32) << 8) + (result[13] as u32), 16);
+
+            self.c21 = get_twos_complement(((result[14] as u32) << 8) + (result[15] as u32), 16);
+
+            self.c30 = get_twos_complement(((result[16] as u32) << 8) + (result[17] as u32), 16);
         }
     }
     
